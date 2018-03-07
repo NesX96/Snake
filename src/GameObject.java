ba6c415d26407cfd65c5396b2d0b2a9db5c596f9
nesis <addis.paoloe@hotmail.it>
2018-03-07 10:47:06 +0100

    Classes

--- src/GameObject.java
+++ src/GameObject.java
@@ -1,2 +1,44 @@
 public class GameObject {
+
+    public collision(GameObject eater, GameObject eaten){
+
+    }
+
+    public setX(){
+
+    }
+
+    public setY(){
+
+    }
+
+    public getX(){
+
+    }
+
+    public getY(){
+
+    }
+
+    public setSpeed(int speed){
+
+    }
+
+    public getSpeed(){
+
+    }
+
+    public abstract tick(){
+
+    }
+
+    public abstract render(){
+
+    }
+
+    public abstract getBounds(){
+
+    }
+
+
 }
--- src/Inanimate.java
+++ src/Inanimate.java
@@ -1,2 +1,12 @@
 public class Inanimate extends GameObject {
+
+
+    public abstract move(){
+
+    }
+
+    private abstract eaten(){
+
+    }
+
 }
