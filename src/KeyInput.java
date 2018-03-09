40ea472cd88a73643be262b0c2c123ebde1673e1
nesis <addis.paoloe@hotmail.it>
2018-03-09 19:58:08 +0100

    Added KeyInput added keyReleased

--- src/KeyInput.java
+++ src/KeyInput.java
@@ -4,6 +4,7 @@ import java.awt.event.KeyEvent;
 public class KeyInput extends KeyAdapter {
 
     private Handler handler;
+    private boolean keyPressed = false;
 
     public KeyInput(Handler handler){
         this.handler = handler;
@@ -13,18 +14,31 @@ public class KeyInput extends KeyAdapter {
 
         int key = e.getKeyCode();
 
-        if(key == KeyEvent.VK_UP){
-            handler.setPlayerDirection(Direction.UP);
+        if(!keyPressed){
 
-        } else if (key == KeyEvent.VK_RIGHT){
-            handler.setPlayerDirection(Direction.RIGHT);
+            if(key == KeyEvent.VK_UP){
+                handler.setPlayerDirection(Direction.UP);
+                keyPressed = true;
+            } else if (key == KeyEvent.VK_RIGHT){
+                handler.setPlayerDirection(Direction.RIGHT);
+                keyPressed = true;
+            } else if (key == KeyEvent.VK_DOWN){
+                handler.setPlayerDirection(Direction.DOWN);
+                keyPressed = true;
+            } else if (key == KeyEvent.VK_LEFT){
+                handler.setPlayerDirection(Direction.LEFT);
+                keyPressed = true;
+            }
 
-        } else if (key == KeyEvent.VK_DOWN){
-            handler.setPlayerDirection(Direction.DOWN);
+        }
+    }
 
-        } else if (key == KeyEvent.VK_LEFT){
-            handler.setPlayerDirection(Direction.LEFT);
+    public void keyReleased(final KeyEvent e) {
 
+        int key = e.getKeyCode();
+
+        if(key == KeyEvent.VK_UP || key == KeyEvent.VK_RIGHT || key == KeyEvent.VK_DOWN || key == KeyEvent.VK_LEFT ){
+            keyPressed = false;
         }
 
     }
