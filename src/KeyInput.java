b7f64809f065f9978359cc4cfc540d20c194592b
Nabbith <37439162+Nabbith@users.noreply.github.com>
2018-03-19 14:10:14 +0100

    docs

--- src/KeyInput.java
+++ src/KeyInput.java
@@ -3,6 +3,10 @@ import java.awt.event.KeyEvent;
 
 public class KeyInput extends KeyAdapter {
 
+    // https://docs.oracle.com/javase/tutorial/uiswing/misc/keybinding.html
+    // https://stackoverflow.com/questions/22741215/how-to-use-key-bindings-instead-of-key-listeners
+    // https://docs.oracle.com/javase/7/docs/api/javax/swing/KeyStroke.html
+    
     private Handler handler;
     private boolean[] keysPressed;
 
@@ -15,6 +19,10 @@ public class KeyInput extends KeyAdapter {
         keysPressed[e.getKeyCode()] = true;
         update();
     }
+    
+    public void keyPressed(KeyEvent e){
+        keysPressed[e.getKeyCode()] = false;
+    }
 
     private void update() {
     	
@@ -22,27 +30,26 @@ public class KeyInput extends KeyAdapter {
     		if (handler.getPlayer().getDirection() != Direction.DOWN) {
     		 handler.setPlayerDirection(Direction.UP);
     		}
-    		keysPressed[KeyEvent.VK_UP] = false;
     	}
     	if (keysPressed[KeyEvent.VK_RIGHT]) {
     		if (handler.getPlayer().getDirection() != Direction.LEFT) {
     		 handler.setPlayerDirection(Direction.RIGHT);
     		}
-    		keysPressed[KeyEvent.VK_RIGHT] = false;
     	}
     	if (keysPressed[KeyEvent.VK_DOWN]) {
     		if (handler.getPlayer().getDirection() != Direction.UP) {
     		 handler.setPlayerDirection(Direction.DOWN);
     		}
-    		keysPressed[KeyEvent.VK_DOWN] = false;
     	}
     	if (keysPressed[KeyEvent.VK_LEFT]) {
     		if (handler.getPlayer().getDirection() != Direction.RIGHT) {
     		 handler.setPlayerDirection(Direction.LEFT);
     		}
-    		keysPressed[KeyEvent.VK_LEFT] = false;
     	}
     }
+    
+    private void changeDirectionOnLastKey() {
+        
     
 
 
